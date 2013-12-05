c0a3ffecc16b5303f8a87b323ed9c9c46af81a71
wagos <Work5Must4Die3>
2013-12-05 05:07:37 +0300

    - added user dcore

--- src/main/java/org/springframework/samples/petclinic/model/User.java
+++ src/main/java/org/springframework/samples/petclinic/model/User.java
@@ -1,5 +1,8 @@
 package org.springframework.samples.petclinic.model;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +22,12 @@ public class User extends NamedEntity {
     @ManyToMany(fetch = FetchType.EAGER)
     private List<ThemeContent> finishedThemeContentList = new ArrayList<>();
 
+    @ManyToMany
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Task> finishedTasks = new ArrayList<>();
+
+    private int score;
+
     public String getUsername() {
         return username;
     }
@@ -33,5 +42,21 @@ public class User extends NamedEntity {
 
     public void setFinishedThemeContentList(List<ThemeContent> finishedThemeContentList) {
         this.finishedThemeContentList = finishedThemeContentList;
+    }
+
+    public List<Task> getFinishedTasks() {
+        return finishedTasks;
+    }
+
+    public void setFinishedTasks(List<Task> finishedTasks) {
+        this.finishedTasks = finishedTasks;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 }
